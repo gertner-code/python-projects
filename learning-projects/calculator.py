7b11c6aa14c5dd46c4ac5fb05a8f43ed710357ad
gertner-code <32581596+gertner-code@users.noreply.github.com>
2017-10-06 16:20:04 -0400

    Create calculator.py
    a basic calculator program using eval() and regex to remove any attempt at code injection

---
+++ learning-projects/calculator.py
@@ -0,0 +1,37 @@
+import re
+
+
+print ("The Calculator")
+print ("Type 'stop' to exit\n")
+
+
+previous=0
+run= True
+
+def perform_math():
+    global run
+    global previous
+    equation=""
+    
+    
+    if previous == 0:
+        equation= input("Enter equation:")
+    else:
+        equation=input(str(previous))
+        
+    if equation == "stop":
+        print("Bye")
+        run= False
+    else:
+        equation=re.sub('[a-zA-Z,.:;()" "]', '', equation)
+        
+        if previous==0:
+            previous=eval(equation)
+        else:
+            previous=eval(str(previous) + equation)
+        
+        
+
+
+while run:
+    perform_math()
